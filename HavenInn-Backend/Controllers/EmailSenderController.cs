50296754b1c8ba0f4adf02e4a92638b4806ac1a7
Naveen0503 <109223016+Naveen0503@users.noreply.github.com>
2022-09-29 20:52:37 +0530

    Email Notifications for user and guest

--- HavenInn-Backend/Controllers/EmailSenderController.cs
+++ HavenInn-Backend/Controllers/EmailSenderController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System;
 using HavenInn_Library.Interface;
+using HavenInn_Library.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Email02.Controllers
 {
@@ -11,16 +14,51 @@ namespace Email02.Controllers
     [ApiController]
     public class EmailSenderController : ControllerBase
     {
-
+        private readonly HavenInnContext _context;
         IEmailSender _emailSender;
-        public EmailSenderController(IEmailSender emailSender)
+        public EmailSenderController(IEmailSender emailSender,HavenInnContext context)
         {
             _emailSender = emailSender;
+            _context = context;
         }
 
         [HttpPost, Route("SendEmail")]
         public async Task<IActionResult> SendEmailAsync(string recipientEmail, string recipientFirstName, string Body)
         {
+
+            try
+            {
+                string messageStatus = await _emailSender.SendEmailAsync(recipientEmail, recipientFirstName, Body);
+                return Ok(messageStatus);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost, Route("SendEmailtoguest")]
+        public async Task<IActionResult> SendEmailAsynctoguest(/*string recipientEmail, string recipientFirstName*/ string Body,int Guestid)
+        {
+            
+            string recipientEmail= _context.Guest.Where(g=>g.GuestId==Guestid).Select(e=>e.Email).FirstOrDefault().ToString();
+            string recipientFirstName = _context.Guest.Where(g => g.GuestId == Guestid).Select(e => e.Name).FirstOrDefault().ToString();
+            try
+            {
+                string messageStatus = await _emailSender.SendEmailAsync(recipientEmail, recipientFirstName, Body);
+                return Ok(messageStatus);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+        [HttpPost, Route("SendEmailtoUser")]
+        public async Task<IActionResult> SendEmailAsynctoUser(/*string recipientEmail, string recipientFirstName*/ string Body, int Userid)
+        {
+
+            string recipientEmail = _context.User.Where(u => u.UserId == Userid).Select(e => e.Email).FirstOrDefault().ToString();
+            string recipientFirstName = _context.User.Where(g => g.UserId == Userid).Select(e => e.Staff.FirstName).FirstOrDefault().ToString();
             try
             {
                 string messageStatus = await _emailSender.SendEmailAsync(recipientEmail, recipientFirstName, Body);
